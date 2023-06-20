bd3c1a7140865910c4d6f56ed136a4c713d96e42
thomas <kigardetom2001@gmail.com>
2023-06-20 21:12:46 +0300

    🚀 add Review for each products in Dynamic nested routes

---
+++ src/routes/product/[productId]/review/[reviewId]/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+    import { page } from '$app/stores'
+    const { productId, reviewId } = $page.params 
+</script>
+
+<h1>This product of ID: { productId } has reviews of id { reviewId }</h1>
