d9c53dbc202125b27b703e361f7710f664671fe3
thomas <kigardetom2001@gmail.com>
2023-06-20 22:09:03 +0300

    serve pages optionally. Different language pages with an optional one

---
+++ src/routes/docs/[...slug]/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+    import { page } from '$app/stores'
+
+    const slugArr = $page.params.slug.split('/')
+    console.log(slugArr)
+</script>
+
+
+
+
+<h1>Docs</h1>
+{ #if slugArr.length === 1 }
+
+    <p>Viewing docs for feature {slugArr[0]}</p>
+
+{ :else if slugArr.length > 1 }
+
+    <p>Viewing docs for feature:</p>
+    {#each slugArr as feat }
+        <p>{ feat }</p>
+    {/each}
+
+{/if}
+
---
+++ src/routes/marketing/[[lang]]/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+    import { page } from '$app/stores'
+
+    let greetings = {
+        en: 'Hello',
+        es: 'Hola',
+        fr: 'Bonjour',
+        de: 'Hallo',
+        it: 'Ciao',
+        pt: 'Olá',
+    }
+
+    let { lang='it' } = $page.params
+</script>
+
+<h2> { greetings[lang] }</h2>
+
+<style>
+    h2 {
+        color: chocolate;
+    }
+</style>
