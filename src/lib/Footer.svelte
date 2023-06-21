fc732ea424277abbda4cc783da27372481119f17
thomas <kigardetom2001@gmail.com>
2023-06-21 11:25:32 +0300

    separate components

---
+++ src/lib/Footer.svelte
@@ -0,0 +1,13 @@
+
+<footer>
+    <h1>Footer</h1>
+    <h5>Simple Footer</h5>
+</footer>
+
+<style>
+    footer {
+        background-color: #333;
+        color: #fff;
+        padding: 15px;
+    }
+</style>
---
+++ src/lib/Header.svelte
@@ -0,0 +1,12 @@
+<header>
+    <h1>Header</h1>
+    <h5>Simple app</h5>
+</header>
+
+<style>
+     header {
+        background-color: #333;
+        color: #fff;
+        padding: 15px;
+    }
+</style>
