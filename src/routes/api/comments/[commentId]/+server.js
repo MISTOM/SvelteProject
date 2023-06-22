9bce39310492556f60d9ec0dd41f13a1e8ec5f95
thomas <kigardetom2001@gmail.com>
2023-06-22 11:27:30 +0300

    implement delete api route

--- src/routes/api/comments/[commentId]/+server.js
+++ src/routes/api/comments/[commentId]/+server.js
@@ -23,3 +23,14 @@ export async function PATCH (requestEvent) {
     comment.text = text
     return json(comment, { status: 201 } )
 }
+
+export async function DELETE (requestEvent) {
+    const { params } = requestEvent
+    const { commentId } = params
+
+    const deletedComment = comments.find( comment => comment.id === parseInt(commentId))
+    const index = comments.findIndex(comment => comment.id === parseInt(commentId))
+    comments.splice(index, 1)
+    
+    return json(deletedComment, { status: 200 })
+}
