7524d3ccd5bfbf4d4510f9fa80e08a66aaac6bec
thomas <kigardetom2001@gmail.com>
2023-06-24 22:37:06 +0300

    add redirect instead of throwing an error

--- src/routes/product/[productId=integer]/+page.server.js
+++ src/routes/product/[productId=integer]/+page.server.js
@@ -1,13 +1,18 @@
-import { error } from '@sveltejs/kit'
+import { error, redirect,  } from '@sveltejs/kit'
 //Server Load Function
 export const load =  async (loadServerEvent) => {
     const { fetch, params } = loadServerEvent
     const { productId } = params
     if (productId > 10 ){
-        throw error(404, {
-            message: 'Oh no! The product you are looking for does not exist.',
-            hint: 'Try searching for a different product or go back to the home page.'
-        })
+        //either throw error or redirect
+
+        // throw error(404, {
+        //     message: 'Oh no! The product you are looking for does not exist.',
+        //     hint: 'Try searching for a different product or go back to the home page.'
+        // })
+
+        throw redirect(302, '/product')
+
     }
     const title = "Product Details"
     const response = await fetch(`http://localhost:4000/products/${productId}`)
