8bc20ec7d1e50af67e9b209e10cde0fd85936f48
thomas <kigardetom2001@gmail.com>
2023-06-24 21:18:36 +0300

    custom error page

--- src/routes/product/[productId=integer]/+page.server.js
+++ src/routes/product/[productId=integer]/+page.server.js
@@ -4,7 +4,10 @@ export const load =  async (loadServerEvent) => {
     const { fetch, params } = loadServerEvent
     const { productId } = params
     if (productId > 10 ){
-        throw error(404,'Oh no! The product you are looking for does not exist.')
+        throw error(404, {
+            message: 'Oh no! The product you are looking for does not exist.',
+            hint: 'Try searching for a different product or go back to the home page.'
+        })
     }
     const title = "Product Details"
     const response = await fetch(`http://localhost:4000/products/${productId}`)
